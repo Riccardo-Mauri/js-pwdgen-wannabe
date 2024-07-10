00104bdf0f6d3121e4cd81545f861a337c40b1e3
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-07-10 16:54:27 +0200

    fix password

--- js/scripts.js
+++ js/scripts.js
@@ -6,28 +6,33 @@
 */
 
 // 1. chiedo il nome all'utente e mi salvo il suo valore
-const name=(prompt('qual è il tuo nome?'))
-console.log('name', name);
+const ElementName=(prompt('qual è il tuo nome?'))
+console.log('name', ElementName);
 
 //2. chiedo il suo cognome e mi salvo il suo valore
-const surname=(prompt('Qual è il tuo cognome?'))
-console.log('surname',surname);
+const ElementSurname=(prompt('Qual è il tuo cognome?'))
+console.log('surname',ElementSurname);
 
 //3.chiedo ora il suo colore preferito
-const color=(prompt('Qual è i ltuo colore preferito?'))
-console.log('color',color);
+const ElementColor=(prompt('Qual è il tuo colore preferito?'))
+console.log('color',ElementColor);
 
 //4. inserirsco ora nella pagina il risultato + 130 - VERIFICA IN CONSOLE
-console.log('Il tuo nome è:' + name);
-console.log('Il tuo cognome è:' + surname);
-console.log('Il tuo nome è:' + color, + 130);
+console.log('Il tuo nome è:' + ElementName);
+console.log('Il tuo cognome è:' + ElementSurname);
+console.log('Il tuo nome è:' + ElementColor);
 
 //4. inserirsco ora nella pagina il risultato + 130
-document.getElementById('result_name').innerHTML = 'Il tuo Nome è:' + name;
-document.getElementById('result_surname').innerHTML = 'Il tuo Cognome è:' + surname;
-document.getElementById('result_color').innerHTML = 'Il tuo Colore preferito è:' + color + 130;
+document.getElementById('result_name').innerHTML = 'Il tuo Nome è:' + ElementName;
+document.getElementById('result_surname').innerHTML = 'Il tuo Cognome è:' + ElementSurname;
+document.getElementById('result_color').innerHTML = 'Il tuo Colore preferito è:' + ElementColor;
 
 //4.5 inserisco il risultato come stringa nel HTML 
-document.writeln('Il tuo Nome è:' + name);
-document.writeln('Il tuo Cognome è:' + surname);
-document.writeln('Il tuo Colore preferito è:' + color + 130);
+document.writeln('Il tuo Nome è:' + ElementName);
+document.writeln('Il tuo Cognome è:' + ElementSurname);
+document.writeln('Il tuo Colore preferito è:' + ElementColor);
+
+
+console.log('La tua password è:' + ElementName + ElementSurname + ElementColor + 130);
+
+document.getElementById('password').innerHTML = 'La tua password è:' + ElementName + ElementSurname + ElementColor + 130;
